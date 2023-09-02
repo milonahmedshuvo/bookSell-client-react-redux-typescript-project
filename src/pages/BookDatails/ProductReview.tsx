953fa07515bc39ab0589b69103b024a3c4e50ab6
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-09-02 23:56:26 +0600

    create function

--- src/pages/BookDatails/ProductReview.tsx
+++ src/pages/BookDatails/ProductReview.tsx
@@ -1,16 +1,20 @@
-
-
 const ProductReview = () => {
-
-
   return (
     <div className=" mt-20">
-        <form className="flex justify-center">
-        <input type="text" placeholder="Comments" className="input input-bordered input-sm w-full md:w-1/2 " />
-        <input type="submit" value="Comment" className="bg-blue-400 py-1 text-white ml-1 px-6 rounded" />
-        </form>  
+      <form onSubmit={handleReview} className="flex justify-center">
+        <input
+          type="text"
+          placeholder="Comments"
+          className="input input-bordered input-sm w-full md:w-1/2 "
+        />
+        <input
+          type="submit"
+          value="Comment"
+          className="bg-blue-400 py-1 text-white ml-1 px-6 rounded"
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default ProductReview
+export default ProductReview;
