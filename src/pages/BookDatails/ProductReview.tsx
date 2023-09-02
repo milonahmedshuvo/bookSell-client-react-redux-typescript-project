01daf19ac6c3d165d252d8ea9566028a90f01f30
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-09-02 23:55:38 +0600

    commits

--- src/pages/BookDatails/ProductReview.tsx
+++ src/pages/BookDatails/ProductReview.tsx
@@ -6,8 +6,8 @@ const ProductReview = () => {
   return (
     <div className=" mt-20">
         <form className="flex justify-center">
-        <input type="text" placeholder="Type here" className="input input-bordered input-sm w-full md:w-1/2 " />
-        <input type="submit" value="Comment" className="bg-blue-400 py-1 text-white px-6 rounded" />
+        <input type="text" placeholder="Comments" className="input input-bordered input-sm w-full md:w-1/2 " />
+        <input type="submit" value="Comment" className="bg-blue-400 py-1 text-white ml-1 px-6 rounded" />
         </form>  
     </div>
   )
