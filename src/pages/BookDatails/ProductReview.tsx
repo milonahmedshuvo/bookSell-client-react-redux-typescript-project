991bccd00eb4aa14c3178687a8147b7b105c0f98
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-09-02 23:54:25 +0600

    dising

--- src/pages/BookDatails/ProductReview.tsx
+++ src/pages/BookDatails/ProductReview.tsx
@@ -7,7 +7,7 @@ const ProductReview = () => {
     <div className=" mt-20">
         <form className="flex justify-center">
         <input type="text" placeholder="Type here" className="input input-bordered input-sm w-full md:w-1/2 " />
-        <input type="submit" value="Comment" />
+        <input type="submit" value="Comment" className="bg-blue-400 py-1 text-white px-6 rounded" />
         </form>  
     </div>
   )
